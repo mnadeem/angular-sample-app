777f74360f5dcfaddd5e4a79b9d90707eec70f21
mnadeem <coolmind182006@gmail.com>
2014-10-26 12:41:51 +0530

    Added state providers

--- app/src/config.js
+++ app/src/config.js
@@ -20,4 +20,32 @@ angular
     }
     //disable IE ajax request caching
     $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
- }]); 
+ }]);
+
+/**
+ * @ngdoc overview
+ * @name sample-app
+ * @requires $stateProvider
+ * @description
+ *
+ * Configures the @module sample-app $stateProvider
+ */
+angular
+  .module('sample-app')
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		
+		// For any unmatched url, redirect to /plan
+		$urlRouterProvider.otherwise("/plan");
+
+		$stateProvider.state('plan', {
+			url : '/plan',
+			templateUrl : 'src/plan/main.tpl.html'
+		}).state('details', {
+			url : '/details',
+			templateUrl : 'src/plan/details/main.tpl.html'
+		}).state('summary', {
+			url : '/summary',
+			templateUrl : 'src/plan/summary/main.tpl.html'
+		});
+  }]); 
+
